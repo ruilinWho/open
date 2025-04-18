fe804e7a935ba3a9e27f2ed1ab20186807f159d9
LynN <hrl24@mails.tsinghua.edu.cn>
2025-04-18 17:13:13 +0800

    update

---
+++ data_augmentation/get_multiple_anontations.py
@@ -0,0 +1,358 @@
+import concurrent.futures
+import copy
+import functools
+import json
+import multiprocessing
+import os
+import random
+import re
+import signal
+import sqlite3
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from multiprocessing import Pool
+from pathlib import Path
+
+import func_timeout
+from tqdm import tqdm
+
+data = json.load(open("./bird-train-dynamic-short.json"))
+
+all_db_ids = list(set(dp["db_id"] for dp in data))
+
+DB_BASE = Path("<Anonymous>")
+import openai
+
+
+def retry_with_exponential_backoff(
+    errors: tuple,
+    initial_delay: float = 10,
+    exponential_base: float = 2,
+    jitter: bool = True,
+    max_retries: int = 6,
+):
+    """
+    Retry a function with exponential backoff.
+    :param errors: Tuple of errors to retry on.
+    :param initial_delay: Initial delay in seconds.
+    :param exponential_base: Base for exponential backoff.
+    :param jitter: Add jitter to the delay.
+    :param max_retries: Maximum number of retries.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # Initialize variables
+            num_retries = 0
+            delay = initial_delay
+
+            # Loop until a successful response or max_retries is hit or an exception is raised
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                # Retry on specific errors
+                except errors as e:
+                    print(f"Error: {e}. Retrying in {delay} seconds...")
+                    # Increment retries
+                    num_retries += 1
+                    # Check if max retries has been reached
+                    if num_retries > max_retries:
+                        raise Exception(f"Maximum number of retries ({max_retries}) exceeded.") from None
+                    # Increment the delay
+                    delay *= exponential_base * (1 + jitter * random.random())
+                    # Sleep for the delay
+                    time.sleep(delay)
+                # Raise exceptions for any errors not specified
+                except Exception as e:
+                    raise e
+
+        return wrapper
+
+    return decorator
+
+
+OPENAI_POTENTIAL_ERRORS = (
+    openai.RateLimitError,
+    openai.APIError,
+    openai.APIConnectionError,
+    openai.InternalServerError,
+)
+
+
+@retry_with_exponential_backoff(OPENAI_POTENTIAL_ERRORS)
+def openai_completion_with_backoff(*args, **kwargs) -> str:
+    """Retry the OpenAI completion call with exponential backoff."""
+    assert OPENAI_CLIENT is not None
+    return openai_completion(*args, **kwargs)
+
+
+def openai_completion(
+    prompt: str,
+    model: str = "deepseek-r1-250120",
+    temperature: float = 0.2,
+    top_p: float = 1,
+    max_tokens: int = 8192,
+) -> str:
+    messages = [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}]
+    response = OPENAI_CLIENT.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+        top_p=top_p,
+        n=1,
+        max_tokens=max_tokens,
+    )
+    return response.choices[0].message.content
+
+
+def extract_sql(s: str) -> str:
+    # extract from ```sql and ```
+    pattern = r"```sql\s*(.*?)\s*```"
+    match = re.search(pattern, s, re.DOTALL)
+
+    s_1 = match.group(1) if match else s
+
+    # Try to extract WITH first
+    pattern_with = r"(WITH.*)"
+    match_with = re.search(pattern_with, s_1, re.DOTALL)
+    if match_with:
+        s_2 = match_with.group(1)
+    else:
+        # If no WITH match, try to extract SELECT
+        pattern_select = r"(SELECT.*)"
+        match_select = re.search(pattern_select, s_1, re.DOTALL)
+        s_2 = match_select.group(1) if match_select else s_1
+
+    # Remove all the \t, \n, and continuous spaces
+    s_3 = re.sub(r"\s+", " ", s_2).strip()
+
+    return s_3
+
+
+# @func_timeout.func_set_timeout(30)
+# def exec_on_db(db_path, query):
+#     try:
+#         conn = sqlite3.connect(db_path)
+#         cursor = conn.cursor()
+#         cursor.execute(query)
+#         return cursor.fetchall()
+#     finally:
+#         conn.close()
+
+
+def exec_on_db(db_path, query):
+    # 普通的执行，不用 func_timeout
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+def run_query_with_timeout(db_path, query, timeout=30):
+    executor = concurrent.futures.ProcessPoolExecutor(max_workers=1)
+    future = executor.submit(exec_on_db, db_path, query)
+    try:
+        result = future.result(timeout=timeout)
+        return result
+    except concurrent.futures.TimeoutError:
+        print("TimeoutError: ", query)
+        # 尝试杀掉子进程
+        for proc in executor._processes.values():
+            try:
+                os.kill(proc.pid, signal.SIGTERM)
+                print(f"Killed process with PID: {proc.pid}")
+            except Exception as e:
+                print(f"Failed to kill process {proc.pid}: {e}")
+        raise func_timeout.FunctionTimedOut("Query execution timed out.")
+    finally:
+        # 关闭 executor，并尽快回收资源
+        executor.shutdown(wait=False)
+
+
+def format_result(r: str | list):
+    if type(r) is str and r[:6] == "Error:":
+        return r
+
+    result = copy.deepcopy(r)
+
+    if len(result) == 0:
+        formatted = "The execution result is empty."
+    elif len(result) <= 10:
+        formatted = f"The execution result contains {len(result)} rows.\n"
+        for i in range(len(result) - 1, -1, -1):
+            if len(str(result[i])) > 200:
+                result[i] = str(result[i])[:200] + "..."
+        formatted += "\n".join([str(row) for row in result])
+    else:
+        original_len = len(result)
+        result = result[:10]
+        for i in range(len(result) - 1, -1, -1):
+            if len(str(result[i])) > 200:
+                result[i] = str(result[i])[:200] + "..."
+
+        formatted = f"The execution result contains {original_len} rows. (showing the top 10 rows)\n"
+        formatted += "\n".join([str(row) for row in result])
+    return formatted
+
+
+COT_TEMPLATE = """\
+For a Text-to-SQL scenario, I will provide you:
+- a database schema
+- a user question
+- some correct SQL queries that can answer the user's question
+- some wrong SQL queries that cannot answer the user's question
+
+Your task is to generate a new SQL query to answer the user's question.
+Requirements:
+- The new SQL query should be a valid SQLite query (do not use too complex SQL syntax)
+- The new SQL query should have a different solution approach from the given correct SQL queries.
+    - different solution approach means the new SQL query should not be a simple modification of the given correct SQL queries. You should try to change the overall SQL structure, not just change the alias or table order.
+    - The new SQL query should be a valid SQL query and return the same result as the given correct SQL queries.
+    - for example, you may solve a question with multuple solution approaches: 
+        1. multi-table JOIN
+        2. common table expressions(CTE) and WITH clause
+        3. subqueries
+        4. other approaches
+- The new SQL query should answer the user's question and return the same result as the given correct SQL queries.
+- Your output should generate the same result as the correct output. 
+- You should also pay attention to the wrong SQL queries and avoid making the same mistake. (For example if a wrong SQL query times out, you should not generate a similar SQL query that also times out.)
+- If you can find a SQL with a different solution approach, return it with ```sql and ``` tags.
+
+Note:
+- You should avoid generating SQL queries that are too similar to existing correct SQL queries.
+- If you cannot find a SQL with a different solution approach or you think you have to use unnatural syntax, please return "None".
+- If you find the given correct SQL queries already cover all reasonable solutions and you have to use inefficient SQL queries to generate a new one, please return "None".
+- ** You should focus on generating high-quality SQL queries (suitable for the question) and avoid generating inefficient SQL queries or too verbose SQL queries. **
+    - Inefficient SQL examples: (1) Cartesian Product (2) Too deep nested subqueries (3) Improper IN or EXISTS usage (such as EXISTS 1) (4) Subquery with SELECT 1
+    - DO NOT generate those inefficient verbose SQL queries. We would rather accept "None".
+    - We want elegant and efficient SQL queries that can solve the problem effectively.
+
+[Database Schema]
+{schema}
+[User question]
+{question}
+[Correct Output]
+{correct_output}
+[Correct SQL queries]
+{correct_sqls}
+[Wrong SQL queries]
+{wrong_sqls}
+"""
+
+
+def generate_multiple_sqls(datapoint):
+    schema = datapoint["schema"]
+    question = datapoint["question"]
+    evidence = datapoint["evidence"]
+
+    if len(evidence) > 5:
+        question += f"\nHint: {evidence}"
+
+    gt_sql = datapoint["query"]
+    if gt_sql[-1] != ";":
+        gt_sql += ";"
+
+    correct_sqls = [gt_sql]
+    wrong_sqls = []
+    timeout_sqls = []
+
+    db_id = datapoint["db_id"]
+    db_path = DB_BASE / db_id / f"{db_id}.sqlite"
+
+    try:
+        gold_result = run_query_with_timeout(db_path, gt_sql)
+    except Exception as e:
+        print("@Gold SQL Exception: ", e)
+        return None
+    except func_timeout.FunctionTimedOut:
+        datapoint["correct_sqls"] = [gt_sql]
+        datapoint["wrong_sqls"] = []
+        return datapoint
+
+    if len(gold_result) == 0:
+        datapoint["correct_sqls"] = [gt_sql]
+        datapoint["wrong_sqls"] = []
+        return datapoint
+
+    for _ in range(5):
+        correct_text = ""
+        for id, sql in enumerate(correct_sqls):
+            correct_text += f"[correct SQL {id + 1}] {sql}\n"
+        wrong_text = ""
+        for id, sql in enumerate(wrong_sqls):
+            wrong_text += f"[wrong SQL {id + 1}] {sql}\n"
+
+        if len(wrong_sqls) == 0:
+            wrong_text = "No wrong SQL query yet."
+
+        prompt = COT_TEMPLATE.format(
+            schema=schema,
+            question=question,
+            correct_output=format_result(gold_result),
+            correct_sqls=correct_text,
+            wrong_sqls=wrong_text,
+        )
+        # response = openai_completion(prompt)
+        response = openai_completion_with_backoff(prompt)
+
+        if "None" in response:
+            break
+
+        pred_sql = extract_sql(response)
+
+        try:
+            if pred_sql in timeout_sqls:
+                continue
+
+            pred_result = run_query_with_timeout(db_path, pred_sql)
+            if set(pred_result) == set(gold_result):
+                correct_sqls.append(pred_sql)
+            else:
+                wrong_sqls.append(pred_sql + "(Wrong Result)")
+        except Exception as e1:
+            wrong_sqls.append(pred_sql + f"(Exception: {e1})")
+            continue
+        except func_timeout.FunctionTimedOut:
+            wrong_sqls.append(pred_sql + "(Timeout)")
+            timeout_sqls.append(pred_sql)
+            continue
+
+        if len(correct_sqls) >= 3:
+            break
+
+    # add "correct_sqls" and "wrong_sqls" to datapoint
+    datapoint["correct_sqls"] = correct_sqls
+    datapoint["wrong_sqls"] = wrong_sqls
+
+    return datapoint
+
+
+if __name__ == "__main__":
+    all_cot_data = []
+    cot_data_lock = threading.Lock()
+
+    # Save Directory
+    save_dir = Path("./test")
+    save_dir.mkdir(exist_ok=True)
+
+    result_count = 0
+
+    with ThreadPoolExecutor(max_workers=30) as executor:
+        futures = [executor.submit(generate_multiple_sqls, datapoint) for datapoint in data[100:105]]
+        for future in tqdm(as_completed(futures), total=len(futures)):
+            result = future.result()
+            result_count += 1
+            print(f"Result Count: {result_count}")
+            if result is not None:
+                with cot_data_lock:
+                    all_cot_data.append(result)
+
+                    if len(all_cot_data) % 100 == 0:
+                        with open(save_dir / f"spider-train-dynamic-short-multiple_{len(all_cot_data)}.json", "w") as f:
+                            json.dump(all_cot_data, f, indent=2)
+
+    # Save results
+    with open(save_dir / "test.json", "w") as f:
+        json.dump(all_cot_data, f, indent=2)
---
+++ data_augmentation/obtain_intermediate_representation.py
@@ -0,0 +1,265 @@
+import json
+import os
+import sqlite3
+from pathlib import Path
+
+import chardet
+import pandas as pd
+from langchain.utilities.sql_database import SQLDatabase
+
+
+def string_equivalent(str1: str, str2: str) -> bool:
+    """
+    Compare two strings for "equality".
+    Complete equality or differences only in spaces and underscores are considered equal.
+
+    :param str1: The first string
+    :param str2: The second string
+    :return: Return True if the strings are considered equal, otherwise return False
+    """
+    if str1 == str2:
+        return True
+
+    normalized_str1 = "".join(char.lower() for char in str1 if char not in " _`")
+    normalized_str2 = "".join(char.lower() for char in str2 if char not in " _`")
+    return normalized_str1 == normalized_str2
+
+
+class Schema2IR:
+    """
+    Input information of a database schema, convert it to a dict representation of the schema.
+    Thie intermediate representation (IR) can then be used for schema linking.
+
+    IR format:
+    - db_id
+    - db_dir
+    - db_json
+    - bench
+    - tables
+        - table_id
+        - table_name
+    """
+
+    ir: dict[str, any]
+
+    def __init__(self, db_id: str, db_dir: Path, db_json: dict, bench: str):
+        self.db_id = db_id
+        self.db_dir = db_dir
+        self.db_json = db_json
+        self.bench = bench
+
+        # assert self.bench in ["spider", "bird_train", "bird_dev"]
+        self.ir = {"db_id": db_id, "bench": bench}
+
+        self._parse_tables()
+
+    def _parse_tables(self):
+        self.ir["tables"] = []
+        for table_idx, table_name in enumerate(self.db_json["table_names"]):
+            original_table_name = self.db_json["table_names_original"][table_idx]
+
+            table = {
+                "table_idx": table_idx,
+                "table_name": original_table_name,
+                "table_comment": f" -- {table_name}" if not string_equivalent(table_name, original_table_name) else "",
+            }
+
+            columns, primary_keys = self._parse_columns(table_idx, original_table_name)
+            table["columns"] = columns
+            table["primary_keys"] = primary_keys
+            table["foreign_keys"] = self._parse_foreign_keys(table_idx, original_table_name)
+            table["value_examples"] = self._parse_value_examples(original_table_name)
+            self.ir["tables"].append(table)
+
+    def _parse_columns(self, table_idx: int, table_name: str) -> list[dict[str, any]]:
+        # connect to db
+        db_path = self.db_dir / f"{self.db_id}.sqlite"
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+
+        column_types = {}
+        column_null_able = {}
+        column_is_primary_key = {}
+        column_contain_null = {}
+        cursor.execute(f'PRAGMA table_info("{table_name}")')
+
+        # get (type | nullable | primary key)
+        for col in cursor.fetchall():
+            column_name = col[1]
+            column_type = col[2]
+            column_types[column_name] = column_type
+            column_null_able[column_name] = col[3]
+            if col[5] >= 1:
+                column_is_primary_key[column_name] = True
+
+            # Check if the column contains NULL
+            sql = f'SELECT COUNT(*) FROM "{table_name}" WHERE "{column_name}" IS NULL'
+            cursor.execute(sql)
+            column_contain_null[column_name] = cursor.fetchone()[0] > 0
+
+        # get UNIQUE columns
+        column_unique = {}
+        cursor.execute(f'PRAGMA index_list("{table_name}")')
+        for index in cursor.fetchall():
+            if index[2] == 1:
+                for column in cursor.execute(f'PRAGMA index_info("{index[1]}")').fetchall():
+                    column_name = column[2]
+                    column_unique[column_name] = True
+        conn.close()
+
+        # Prepare description for BIRD
+        if self.bench == "bird_train" or self.bench == "bird_dev":
+            desc_path = self.db_dir / "database_description" / f"{table_name}.csv"
+            if not desc_path.exists():
+                raise ValueError(f"Description file {desc_path} does not exist.")
+            with open(desc_path, "rb") as f:
+                result = chardet.detect(f.read())
+            desc_df = pd.read_csv(desc_path, encoding=result["encoding"])
+
+        # make column list and primary key list
+        columns = []
+        primary_keys = []
+
+        for col_idx, (col_table_idx, col_name) in enumerate(self.db_json["column_names"]):
+            if col_table_idx == table_idx:
+                original_col_name = self.db_json["column_names_original"][col_idx][1]
+
+                # Type
+                col_type = column_types[original_col_name].upper()
+                if "INT" in col_type:
+                    col_type = "INT"
+
+                # Nullable and UNIQUE
+                column_def = f'    "{original_col_name}" {col_type}'
+                if (column_null_able[original_col_name]) and (original_col_name not in column_is_primary_key):
+                    column_def += " NOT NULL"
+                if (original_col_name in column_unique) and (original_col_name not in column_is_primary_key):
+                    column_def += " UNIQUE"
+
+                not_primary_and_nullable = bool(
+                    (original_col_name not in column_is_primary_key) and (not column_null_able[original_col_name])
+                )
+
+                # Column comment and description
+                if self.bench == "spider":
+                    if not string_equivalent(col_name, original_col_name):
+                        column_def += f" -- {col_name}"
+                else:
+                    cmt = " -- "
+                    if not string_equivalent(col_name, original_col_name):
+                        cmt += col_name
+
+                    # Add description
+                    if self.bench != "bird_train" and self.bench != "bird_dev":
+                        col_desc = None
+                    else:
+                        desc_line = desc_df[desc_df["original_column_name"].str.strip() == original_col_name]
+                        if not desc_line.empty:
+                            col_desc = desc_line["column_description"].values[0]
+
+                    if (
+                        col_desc
+                        and not pd.isna(col_desc)
+                        and not string_equivalent(col_name, col_desc)
+                        and not string_equivalent(original_col_name, col_desc)
+                    ):
+                        cmt += f"({col_desc})"
+
+                    if cmt != " -- ":
+                        column_def += cmt
+
+                if original_col_name in column_is_primary_key:
+                    primary_keys.append(len(columns))
+
+                columns.append(
+                    {
+                        "col_idx": len(columns),
+                        "col_name": original_col_name,
+                        "col_defination": column_def,
+                        "not_primary_and_nullable": not_primary_and_nullable,
+                        "contain_null": column_contain_null[original_col_name],
+                    }
+                )
+
+        return columns, primary_keys
+
+    def _parse_foreign_keys(self, table_idx: int, table_name: str) -> list[dict[str, any]]:
+        """Get foreign key information for a specific table."""
+        foreign_keys = []
+        db = self.db_json
+        for fk in db["foreign_keys"]:
+            if db["column_names"][fk[0]][0] == table_idx:
+                foreign_keys.append(
+                    {
+                        "table": f'"{table_name}"',
+                        "column": f'"{db["column_names_original"][fk[0]][1]}"',
+                        "referenced_table": db["table_names_original"][db["column_names"][fk[1]][0]],
+                        "referenced_column": f'"{db["column_names_original"][fk[1]][1]}"',
+                    }
+                )
+        return foreign_keys
+
+    def _parse_value_examples(self, table_name: str) -> dict[str, any]:
+        db = SQLDatabase.from_uri(f"sqlite:///{os.path.join(self.db_dir, self.db_id + '.sqlite')}")
+
+        # Column names
+        column_info = db._execute(f"SELECT name FROM pragma_table_info('{table_name}');")
+        column_names = [col["name"] for col in column_info]
+
+        # For every column, execute a query to get a sample of data
+        column_data = {}
+        for col_name in column_names:
+            result = db._execute(f'SELECT DISTINCT "{col_name}" FROM "{table_name}" LIMIT 3')
+            if not result:
+                continue
+
+            column_values = []
+            for row in result:
+                column_values.append(row[col_name])
+
+            # Not too long
+            column_values = [value for value in column_values if len(str(value)) < 80 and value]
+            if not column_values:
+                continue
+            column_data[col_name] = column_values
+
+        return column_data
+
+    def to_dict(self):
+        return self.ir
+
+
+if __name__ == "__main__":
+    SPIDER_BASE = Path().cwd().parent / "dataset" / "spider"
+    BIRD_TRAIN_BASE = Path().cwd().parent / "dataset" / "bird-train"
+    BIRD_DEV_BASE = Path().cwd().parent / "dataset" / "bird-dev"
+
+    # benchs = ["bird_train"]
+    # benchs = ["bird_dev"]
+    # benchs = ["spider"]
+    # benchs = [
+    #     "DB_schema_synonym",
+    #     "DB_schema_abbreviation",
+    #     "DB_DBcontent_equivalence",
+    # ]
+
+    for bench in benchs:
+        DB_BASE = "<Anonymous>"
+        TABLE_PATH = "<Anonymous>"
+
+        with open(TABLE_PATH) as f:
+            tables = json.load(f)
+
+        db_ids = sorted([table["db_id"] for table in tables])
+        ir_set = []
+
+        for db_id in db_ids:
+            db_dir = DB_BASE / db_id
+            db_json = [table for table in tables if table["db_id"] == db_id][0]
+            print(f"### {db_id}")
+            ir = Schema2IR(db_id, db_dir, db_json, bench).to_dict()
+
+            ir_set.append(ir)
+
+        with open(f"./ir_2025/{bench}_ir.json", "w") as f:
+            json.dump(ir_set, f, indent=2)
